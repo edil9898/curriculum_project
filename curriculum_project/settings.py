1e9d7b4420f82fa606e51a373cdfc1fac8138645
edil9898 <avelar.0101@gmail.com>
2024-11-04 16:06:37 -0600

    first commit

--- curriculum_project/settings.py
+++ curriculum_project/settings.py
@@ -40,12 +40,17 @@ STATICFILES_DIRS = [     # Agrega esta línea
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1(ag+d&1dqflpjhk*6%#4(vx4ub(68im39n=r!-vb85v=6mdm@'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='sdsadasdsadasdsad')  
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 
 # Application definition
@@ -69,13 +74,16 @@ TAILWIND_APP_NAME = 'curriculum'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 ROOT_URLCONF = 'curriculum_project.urls'
 
@@ -153,6 +161,10 @@ LOGOUT_REDIRECT_URL = 'login'  # Página a la que redirige después del logout
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STRIPE_SECRET_KEY = 'sk_test_51QHEPgJWRq8I5huEOUq72lRdgGww2tl2ofWaenc9Opc7f4OgN3rERcbnWAUaBSlQrw70qVQOGiaVtnWB7735oK7c00O0ANy3ti'
 STRIPE_PUBLIC_KEY = 'pk_test_51QHEPgJWRq8I5huEIEOTxpOR00IPaIQbLfcnbxzVySNizVhSFJIN0ONE5bwwrLv9UJmYgdCnv36nyWiQn5R1JKKn00UJoeNq9h'
